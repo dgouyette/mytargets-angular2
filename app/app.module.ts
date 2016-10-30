23446517efee9042386ff1803f025084efe0223b
Damien GOUYETTE <damien.gouyette@gmail.com>
2016-10-30 18:09:23 +0100

    add bootstrap

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { TargetComponent }  from './app.component';
-import {TableComponent} from "./table.component";
+import {TableComponent, ScoreService} from "./table.component";
 
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ TargetComponent, TableComponent ],
+  declarations: [ TargetComponent, TableComponent],
   bootstrap: [ TargetComponent, TableComponent]
 })
 export class AppModule { }
--- app/table.component.ts
+++ app/table.component.ts
@@ -2,10 +2,22 @@ import {AfterViewInit, Component} from "@angular/core";
 @Component({
   selector: "targetTable",
   template: `
-    <table style="border: 1px solid black;">
+    <table class="table table-bordered">
     <tr>
-    <td>I'm a table</td>
-</tr>
+      <th>Nombre de flèches</th>
+      <th colspan="3">Point par Flèche</th>
+      <th>Total des 3 flèches</th>
+      <th>Total cumulé</th>
+    </tr>
+    <tr>
+      <td class="text-center">3</td>
+      <td class="text-center">9</td>
+      <td class="text-center">9</td>
+      <td class="text-center">8</td>
+      <td class="text-center"></td>
+      <td class="text-center"></td>
+      <td class="text-center"></td>
+    </tr>
 </table>
     `
 })
@@ -13,5 +25,11 @@ export class TableComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
   }
+
+}
+
+export class ScoreService {
+  scoreHistory: number[];
+
 
 }
