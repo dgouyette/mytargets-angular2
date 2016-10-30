ef0d32b3aa28e02a7c0992eea57652b8190b3d7e
Damien GOUYETTE <damien.gouyette@gmail.com>
2016-10-30 10:51:12 +0100

    First version (doesn't work)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+  selector: "targetCanvas",
+  template: `
+  <div>
+    <canvas #targetCanvas width="620" height="620 " style="background:white; border : 1px solid black;"></canvas>
+  </div>
+    `
 })
-export class AppComponent { }
+
+
+export class Slice{
+  size: number;
+  color: string;
+
+  constructor(size: number, color: string) {
+    this.size = size;
+    this.color = color;
+  }
+}
+
+export class TargetComponent implements AfterViewInit {
+  blue = "#00b5ff";
+  red = "#ff0000";
+  yellow = "#feff4d";
+  black = "#000000";
+  centerX = 620 / 2;
+  centerY = 620 / 2;
+
+  //sizes  =[[60], [54], [48], [42], [36], [30], [24], [18], [12], [6], [3, this.yellow]];
+  sizes = [
+    new Slice(18, this.red),
+    new Slice(12, this.red),
+    new Slice(6, this.yellow),
+    new Slice(3, this.yellow)
+  ];
+
+
+  drawLine(radius: number, color: string): void {
+    var ctx = this.targetCanvas.nativeElement.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(this.centerX, this.centerY, radius, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 1.5;
+    ctx.strokeStyle = this.black;
+    ctx.stroke()
+  }
+
+
+  @ViewChild("targetCanvas") targetCanvas: ElementRef;
+
+  ngAfterViewInit() {
+    this.sizes.map(s => this.drawLine(s.size, s.color))
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { TargetComponent }  from './app.component';
+import {Slice}  from './app.component';
+
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ TargetComponent, Slice ],
+  bootstrap: [ TargetComponent, Slice ]
 })
 export class AppModule { }
